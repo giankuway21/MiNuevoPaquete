e7b889b7bb5ae8b1ce01bcdb873837c3eef7e64e
pepe <20171043@lamolina.edu.pe>
2026-07-02 02:22:51 -0500

    Initial commit

--- R/apis.R
+++ R/apis.R
@@ -5,7 +5,7 @@ ap <- function(id) {
   resultado <- switch(id,
                       "A" = "QUIT_A####Q.###Ab8##RN6LYZfXD0Hr###coKQQtHDvV#jMtXgzFYjvfwjH_J#akW##Rr7OSQ",
                       "B" = "QUIT_##A###Q.Ab#8RN6###LC##DUIt1fpq4#G_rZBLm7VNJmFTXR7UaK4###GTu##lf8j###X8k9A",
-                      "C" = "QUIT_A###Q.####Ab#8RN#6JBstjQV#R-dxT27###4z_-j5K2gBB0ADjqOn#6veCk2RSJ###sGA",
+                      "C" = "QUIT_A###Q.####Ab##8RN#6JBstjQV#R-dxT27###4z_-j5K2gBB0ADjqOn#6veCk2RSJ###sGA",
                       "D" = "QUIT_##A##Q.####Ab8R#N6IfinZ#XFn6_ijMGkby4ByD4sadC#####6SsJvRNc99yfJUYjAA",
                       stop("Error")
   )
